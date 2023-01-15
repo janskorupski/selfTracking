a061396257850dd5cc18f2c7dae38807c9563d19
unknown <PC@DESKTOP-APTVEGF.fw.internal>
2023-01-15 16:11:53 +0100

    pep-ing the variable names and method names

--- Recorder.py
+++ Recorder.py
@@ -19,89 +19,92 @@ class Recorder:
     def __init__(self, maxBlockTime=2 * 60, timeInactive=2 * 60, saveDir="."):
 
         # Parameters (settings)
-        self.maxBlockTime = maxBlockTime
-        self.timeBeforeInactive = timeInactive
-        self.saveDir = saveDir
+        self.max_block_time = maxBlockTime
+        self.time_before_inactive = timeInactive
+        self.save_directory = saveDir
 
-        self.startingTimeOfRecording = time.time()
+        self.starting_time_of_recording = time.time()
 
         # block data
-        self.lastBlockTrigger = time.time()
+        self.last_block_trigger = time.time()
         self.handler = False  # Handler from keyboard package. False indicates no active handler.
-        self.lastWindow = GetWindowText(GetForegroundWindow())
+        self.last_window = GetWindowText(GetForegroundWindow())
         self.handle = False  # Handle from keyboard package - the object returned by handler.
         self.keys_pressed = ""
         self.text_written = ""
-        self.lastDateTime = ""
+        self.last_date_time = ""
 
         # activity data
-        self.active = True
-        self.lastMouseMovement = time.time()
-        self.lastMousePosition = mouse.get_position()
-        self.lastKeyboardStroke = time.time()
-        self.lastTextLength = 0
+        self.last_active_state = True
+        self.last_mouse_movement = time.time()
+        self.last_mouse_position = mouse.get_position()
+        self.last_keyboard_stroke = time.time()
+        self.last_text_length = 0
 
         self.hideText = False
 
     def record(self, maxTime=7 * 24 * 60 * 60):
-        self.startingTimeOfRecording = time.time()
-        self.createBlock()
-        while time.time() - self.startingTimeOfRecording < maxTime:
+        self.starting_time_of_recording = time.time()
+        self.start_block()
+        while time.time() - self.starting_time_of_recording < maxTime:
 
             if keyboard.is_pressed("shift") and keyboard.is_pressed("esc"):
-                self.endBlock()
+                self.end_block()
                 print("exit")
                 break
 
-            self.updateActivity()
+            self.update_activity()
 
-            if self.checkBlockTrigger():
-                self.triggerNewBlock()
+            if self.check_block_trigger():
+                self.trigger_new_block()
 
         time.sleep(0.02)
 
-    def checkActive(self):
+    def check_active(self):
         now = time.time()
-        return self.timeBeforeInactive < now - self.lastKeyboardStroke or \
-               self.timeBeforeInactive < now - self.lastMouseMovement
+        return self.time_before_inactive < now - self.last_keyboard_stroke or \
+               self.time_before_inactive < now - self.last_mouse_movement
 
-    def checkBlockTrigger(self):
-        return self.lastWindow != GetWindowText(GetForegroundWindow()) \
-               or time.time() - self.lastBlockTrigger > self.maxBlockTime \
-               or self.active != self.checkActive()
+    def check_block_trigger(self):
+        if self.last_window != GetWindowText(GetForegroundWindow()) \
+                or time.time() - self.last_block_trigger > self.max_block_time \
+                or self.last_active_state != self.check_active():
+            self.last_active_state = self.check_active()
+            return True
 
-    def startBlock(self):
+    def start_block(self):
         self.handler = keyboard.start_recording()
-        self.lastBlockTrigger = time.time()
-        self.lastWindow = GetWindowText(GetForegroundWindow())
-        self.lastDateTime = str(list(time.localtime())[:-2])[1:-1].replace(", ", ";")
+        self.last_block_trigger = time.time()
+        self.last_window = GetWindowText(GetForegroundWindow())
+        self.last_date_time = str(list(time.localtime())[:-6])[1:-1].replace(", ", ";")
 
-    def endBlock(self):
+    def end_block(self):
         handle = keyboard.stop_recording()
-        self.keys_pressed = self.keysPressed(handle)
-        self.text_written = self.textWritten(handle)
-        self.outPut()
+        self.keys_pressed = self.extract_keys_pressed(handle)
+        self.text_written = self.extract_text_written(handle)
+        self.output()
 
-    def triggerNewBlock(self):
-        self.endBlock()
-        self.startBlock()
+    def trigger_new_block(self):
+        self.end_block()
+        self.start_block()
 
-    def outPut(self):
-        report = self.generateReport()
-        with open(self.saveDir + "\\" + str(self.lastDateTime) + ".txt", "a", encoding="utf-8") as file:
+    def output(self):
+        report = self.generate_report()
+        with open(self.save_directory + "\\" + str(self.last_date_time) + ".txt", "a", encoding="utf-8") as file:
             file.write(report)
 
-    def generateReport(self):
+    def generate_report(self):
         report = ""  # "year;month;day;hour;min;sec;weekday;secStart;secSpent;active;Window;textWritten\n"
-        report += self.lastDateTime + ";" + \
-                  str(self.lastBlockTrigger) + ";" + \
-                  str(time.time() - self.lastBlockTrigger) + ";" + \
-                  str(self.active) + ";" + \
-                  self.lastWindow + ";" + \
+        report += self.last_date_time + ";" + \
+                  str(self.last_block_trigger) + ";" + \
+                  str(time.time() - self.last_block_trigger) + ";" + \
+                  str(self.last_active_state) + ";" + \
+                  self.last_window + ";" + \
                   self.text_written + ";" + \
                   self.keys_pressed + "\n"
+        return report
 
-    def keysPressed(self, handle):
+    def extract_keys_pressed(self, handle):
         keys_pressed = ""
         for event in handle:
             if event.event_type == "down":
@@ -111,7 +114,7 @@ class Recorder:
                     keys_pressed = keys_pressed + "[<semicolon>]"
         return keys_pressed
 
-    def textWritten(self, handle):
+    def extract_text_written(self, handle):
         text_written = ""
         for event in handle:
             if len(event.name) == 1 and event.event_type == "down":
@@ -123,37 +126,37 @@ class Recorder:
                 text_written = text_written[:-1]  # + " [bckspc] "
         return text_written
 
-    def updateActivity(self):
-        if self.lastMousePosition != mouse.get_position():
-            self.lastMouseMovement = time.time()
-            self.lastMousePosition = mouse.get_position()
+    def update_activity(self):
+        if self.last_mouse_position != mouse.get_position():
+            self.last_mouse_movement = time.time()
+            self.last_mouse_position = mouse.get_position()
         if self.handler:
-            if self.lastTextLength != self.handler[0].qsize():
-                self.lastKeyboardStroke = time.time()
-                self.lastTextLength = self.handler[0].qsize()
+            if self.last_text_length != self.handler[0].qsize():
+                self.last_keyboard_stroke = time.time()
+                self.last_text_length = self.handler[0].qsize()
 
-    def setParameters(self):
+    def set_parameters(self):
         pass
 
-    def getParameters(self):
+    def get_parameters(self):
         pass
 
-    def displaySettings(self):
+    def display_settings(self):
         settingString = "|| settings    ||\n"
-        settingString += f"|| refreshrate  : {self.maxBlockTime}\n"
+        settingString += f"|| refreshrate  : {self.max_block_time}\n"
         settingString += f"|| saveRate     : {1}\n"
-        settingString += f"|| timeInactive : {self.timeBeforeInactive}\n"
-        settingString += f"|| saveDir      : {self.saveDir}"
+        settingString += f"|| timeInactive : {self.time_before_inactive}\n"
+        settingString += f"|| saveDir      : {self.save_directory}"
 
         clear_output(wait=True)
         timetime = time.time()
         print(settingString)
-        print("Seconds from last keyboard stroke: " + str(round(timetime - self.lastKeyboardStroke)))
-        print("Seconds from last mouse movement: " + str(round(timetime - self.lastMouseMovement)))
-        print("active: " + str(self.active))
-        print("last report: " + str(round(timetime - self.lastBlockTrigger)))
+        print("Seconds from last keyboard stroke: " + str(round(timetime - self.last_keyboard_stroke)))
+        print("Seconds from last mouse movement: " + str(round(timetime - self.last_mouse_movement)))
+        print("active: " + str(self.last_active_state))
+        print("last report: " + str(round(timetime - self.last_block_trigger)))
 
 
 if __name__ == "__main__":
     rec = Recorder()
-    rec.record(maxTime=40)
+    rec.record(maxTime=30)
