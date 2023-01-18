7e2af82723d60d7ef37a844cf2399c2e6cd0f41f
Jan S <PC@DESKTOP-APTVEGF.fw.internal>
2023-01-18 13:02:27 +0100

    janitor work; removing the unnecessary lines (jupyter notebook experiments residue) and commenting

--- test_data.py
+++ test_data.py
@@ -7,24 +7,20 @@ import pytest
 
 def test_output(monkeypatch):
 
-    def mock_window(*args):
-        return "mock window - aaa.txt"
-
-    monkeypatch.setattr("Recorder.GetWindowText", mock_window )
+    # during testing, we don't want to really record any data (especially since this only works on Windows)
+    monkeypatch.setattr("Recorder.GetWindowText", lambda: "mock window - aaa.txt")
     monkeypatch.setattr("Recorder.GetForegroundWindow", lambda: None)
 
     rec = Recorder.Recorder()
     rec.record(maxTime=3, verbose=False)
 
+    # the expected filename of the log
     file = str(list(time.localtime())[:-6])[1:-1].replace(", ", "-") + ".txt"
 
-    assert file.split(".")[1] == "txt"
-    listed = file[:-4].split("-")
-    assert len(listed) == 3
-    assert not any([not el.isnumeric for el in listed])
-
+    # first, check wether the log even exists
     assert os.path.exists(file)
 
+    # if id does, check if the number of columns matches the expected value
     with open(file, "r", encoding="utf-8") as f:
         assert len(f.readline().split(";")) == 13
 
