ef152f0e99456c6d8f74e19696f07ab91ab24c49
@js407108 <j.skorupski4@student.uw.edu.pl>
2024-06-22 23:40:30 +0200

    Adding the optional flagging of a statistic as safe for future use.

--- src/selfTracking/Statistics.py
+++ src/selfTracking/Statistics.py
@@ -7,14 +7,15 @@ words_only = re.compile('\w+')
 
 class Statistic:
 
-    def __init__(self, name, calc_func):
+    def __init__(self, name, calc_func, is_safe=False):
         self.name = name
         self.calculate = calc_func
+        self.is_safe = is_safe
 
 
 textWritten_stat = Statistic("textWritten", lambda keysPressed, textWritten: textWritten)
 keysPressed_stat = Statistic("keysPressed", lambda keysPressed, textWritten: keysPressed)
-symbolCount_stat = Statistic("symbolCount", lambda keysPressed, textWritten: len(textWritten))
+symbolCount_stat = Statistic("symbolCount", lambda keysPressed, textWritten: len(textWritten), is_safe=True)
 
 def average_word_length(textWritten):
     words = words_only.findall(textWritten)
@@ -23,6 +24,7 @@ def average_word_length(textWritten):
 
 wordLength_stat = Statistic("wordLength_stat",\
                             lambda keysPressed, textWritten:\
-                            mean([len(word) for word in textWritten.replace("\n", " ").split(" ")]))
+                            mean([len(word) for word in textWritten.replace("\n", " ").split(" ")]),
+                            is_safe=True)
 
 all_stats = [textWritten_stat, keysPressed_stat, symbolCount_stat, wordLength_stat]
